d539a80744a7697ecebfa889a41d83821c6e1f16
Michal Pyrka <michal.pyrka@droidsonroids.pl>
2017-05-15 15:19:30 +0200

    Initial Commit

---
+++ SquakersShots/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SquakersShots
+//
+//  Created by Michal Pyrka on 15/05/2017.
+//  Copyright © 2017 Michal Pyrka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
